f1529092163b1fcbebe8ed06351452a2e975b2ff
balaji.jagannathan <balaji.jagannathan@teledirectasia.com>
2020-10-10 19:20:47 +0800

    snapdeal react app

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React, { Fragment } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { createBrowserHistory } from 'history';
+import { ThemeProvider } from '@material-ui/styles';
+import theme from './theme';
+
+// Component.
+import Header from './components/Header';
+const browserHistory = createBrowserHistory();
+
+import {
+    Dashboard,
+    SignIn
+} from './modules';
+
+export default function App() {
+    return (
+        <Fragment>
+            <ThemeProvider theme={theme}>
+                <Header />
+                <Router history={browserHistory} basename="/snapdeal-react-app">
+                    <Switch>
+                        <Route exact path="/">
+                            <SignIn />
+                        </Route>
+                        <Route exact path="/dashboard">
+                            <Dashboard />
+                        </Route>
+                    </Switch>
+                </Router>
+            </ThemeProvider>
+        </Fragment>
+    );
+};
